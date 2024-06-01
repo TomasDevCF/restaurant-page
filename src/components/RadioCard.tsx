c23a86ae2463701cc69dd2a8a3e677a157de9a36
TomasDevCF <tomastiktokb@gmail.com>
2024-06-01 19:19:27 -0300

    edit people form button

--- src/components/RadioCard.tsx
+++ src/components/RadioCard.tsx
@@ -10,7 +10,7 @@ export default function RadioCard({ title }: Props) {
       <input type="radio" defaultChecked={title === "1"} id={title} name="people" value={title} className="hidden peer" />
       <label htmlFor={title} className="inline-flex items-center justify-center w-full p-2 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:border-primary peer-checked:text-primary hover:text-gray-600 hover:bg-gray-100 shadow-xl">
         <div className="block">
-          <div className="w-full text-lg font-semibold text-center">{title == "5" ? "+4" : title}</div>
+          <div className="w-full text-sm sm:text-lg font-semibold text-center">{title == "5" ? "+4" : title}</div>
         </div>
       </label>
     </li>
